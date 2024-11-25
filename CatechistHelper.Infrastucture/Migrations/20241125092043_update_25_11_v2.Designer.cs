8f6a2cef3a7440502e6c86ab8fcdb5e2388d633d
0xSun <ngodinhlong25032001@gmail.com>
2024-11-25 21:34:32 +0700

    update

--- CatechistHelper.Infrastucture/Migrations/20241125092043_update_25_11_v2.Designer.cs
+++ CatechistHelper.Infrastucture/Migrations/20241125092043_update_25_11_v2.Designer.cs
@@ -4,6 +4,7 @@ using CatechistHelper.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CatechistHelper.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241125092043_update_25_11_v2")]
+    partial class update_25_11_v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
