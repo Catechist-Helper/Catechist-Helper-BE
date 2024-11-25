a7ba5fd0128c9022f93e4d01b18e6e295f117ed1
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2024-11-25 19:28:52 +0700

    Add AddParticipantEventRequest

---
+++ CatechistHelper.Domain/Dtos/Requests/Event/AddParticipantEventRequest.cs
@@ -0,0 +1,15 @@
+﻿using CatechistHelper.Domain.Constants;
+using CatechistHelper.Domain.Enums;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace CatechistHelper.Domain.Dtos.Requests.Event
+{
+    public class AddParticipantEventRequest
+    {
+        [Required]
+        public Guid EventId { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
