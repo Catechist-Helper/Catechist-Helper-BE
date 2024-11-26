37495570dbfd4b3145cf44b8455518e1de1a8281
Huynh Minh Thuan <hmtminhthuan@gmail.com>
2024-11-26 08:09:21 +0700

    Update

--- CatechistHelper.Domain/Dtos/Responses/Member/GetMemberResponse.cs
+++ CatechistHelper.Domain/Dtos/Responses/Member/GetMemberResponse.cs
@@ -1,10 +1,12 @@
 ﻿using CatechistHelper.Domain.Dtos.Responses.Account;
+using CatechistHelper.Domain.Dtos.Responses.RoleEvent;
 
 namespace CatechistHelper.Domain.Dtos.Responses.Member
 {
     public class GetMemberResponse
     {
-        public required GetAccountResponse GetAccountResponse { get; set; }
+        public required GetAccountResponse Account { get; set; }
+        public required GetRoleEventResponse RoleEvent { get; set; }
         public Guid RoleEventId { get; set; }
     }
 }
